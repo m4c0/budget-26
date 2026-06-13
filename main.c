3dc760a2e6ab52c8ad2cbc582e8c70d7594af827
Eduardo Costa <m4c0@users.noreply.github.com>
2026-06-13 16:21:20 +0100

    refactoring a bit

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+static char * slurped = NULL;
 static char * slurp(const char * file) {
   FILE * f = fopen(file, "rb");
   assert(f);
@@ -14,43 +15,41 @@ static char * slurp(const char * file) {
   assert(sz);
   assert(0 == fseek(f, 0, SEEK_SET));
 
-  char * data = malloc(sz + 1);
-  assert(1 == fread(data, sz, 1, f));
-  data[sz] = 0;
+  if (slurped) free(slurped);
+  slurped = malloc(sz + 1);
+  assert(1 == fread(slurped, sz, 1, f));
+  slurped[sz] = 0;
 
   fclose(f);
-  return data;
+  return slurped;
 }
 
-static int init() {
+static sqlite3 * db_open() {
   sqlite3 * db;
-
   if (sqlite3_open("budget-26.sqlite", &db)) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
-    return 1;
+    return NULL;
   }
-
-  char * src = slurp("db.sql");
-
-  sqlite3_stmt * stmt;
-  if (sqlite3_prepare_v2(db, src, -1, &stmt, 0)) {
-    fprintf(stderr, "Can't prepare statement: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    free(src);
-    return 1;
-  }
-
-  free(src);
-
-  if (sqlite3_step(stmt) != SQLITE_DONE) {
-    fprintf(stderr, "Can't step statement: %s\n", sqlite3_errmsg(db));
+  return db;
+}
+static int db_exec(sqlite3 * db, const char * sql) {
+  if (sqlite3_exec(db, sql, NULL, NULL, NULL)) {
+    fprintf(stderr, "Can't exec: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
+  sqlite3_close(db);
+  return 0;
+}
 
-  sqlite3_finalize(stmt);
+static int cmd_bank() {
+  return 0;
+}
 
+static int cmd_init() {
+  sqlite3 * db = db_open();
+  if (db_exec(db, slurp("db.sql"))) return 1;
   sqlite3_close(db);
   return 0;
 }
@@ -60,10 +59,9 @@ int main() {
   while (1) {
     printf("> ");
     if (!fgets(buf, sizeof(buf), stdin)) break;
-    if (0 == strncmp(buf, "quit\n", sizeof(buf))) break;
-    else if (0 == strncmp(buf, "init\n", sizeof(buf))) {
-      if (init()) return 1;
-    }
+    if (0 == strcmp(buf, "quit\n")) break;
+    else if (0 == strcmp(buf, "bank\n")) { if (cmd_bank()) return 1; }
+    else if (0 == strcmp(buf, "init\n")) { if (cmd_init()) return 1; }
     else puts("unknown command");
   }
 }
